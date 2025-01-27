74bfba25f964f0c04e3920877cc4609b068ce4ec
AntonKrylosov <windows10.windowsten@gmail.com>
2025-01-27 18:16:23 +0300

    Add files via upload

---
+++ PRAEKT/ТАБЛИЧНЫЕ ЗНАЧЕНИЯ/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]       INT           NOT NULL,
+    [username] VARCHAR (100) NOT NULL,
+    [password] VARCHAR (30)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
